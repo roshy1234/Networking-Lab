f65e332e35929fea0800aa1db0b03eae5c687c29
roshy1234 <jamesroshy@gmail.com>
2025-03-24 12:46:18 +0530

    TCP client and server

---
+++ TCP_client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+
+    char *ip = "127.0.0.1";
+    int port = 8080;
+
+    int sock;
+    struct sockaddr_in addr; 
+    socklen_t addr_size;
+    char buffer[1024];
+    int n;
+    // Create the server socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+    printf("[+]TCP Server Socket Created\n");
+    
+    memset(&addr, '\0', sizeof(server)); /
+    addr.sin_family = AF_INET;
+    addr.sin_port = port
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+    printf("Connected to the server\n");
+
+    bzero(buffer,1024);
+    strcpy(buffer, "Hello.This is client");
+    printf("Client: %s\n", buffer);
+    send(sock,buffer,strlen(buffer),0);
+
+    bzero(buffer,1024);
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+    printf("Disconnected from server\n");
+    return 0;
+}
---
+++ TCP_server.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+
+    char *ip = "127.0.0.1";
+    int port = 8080;
+
+    int server_sock,client_sock;
+    struct sockaddr_in server,client; 
+    socklen_t addr_size;
+    char buffer[1024];
+    int n;
+    // Create the server socket
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_sock < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+    printf("[+]TCP Server Socket Created\n");
+    
+    memset(&server, '\0', sizeof(server)); /
+    server.sin_family = AF_INET;
+    server.sin_port = port
+    server.sin_addr.s_addr = inet_addr(ip);
+    
+    // Bind the socket
+    n = bind(server_sock, (struct sockaddr*)&server, sizeof(server));
+    if(n < 0) {
+        perror("Binding failed");
+        exit(1);
+    }
+    printf("Bind to port %d\n", port);
+
+    // Listen for incoming connections
+    listen(server_sock, 5);
+    printf("Listening...\n");
+    
+    while(1){
+        addr_size = sizeof(client);
+        client_sock = accept(server_sock, (struct sockaddr*)&client, &addr_size);
+        printf("Client connected\n");
+
+        bzero(buffer,1024);
+        recv(client_sock, buffer, sizeof(buffer), 0);
+        printf("Client: %s\n", buffer);
+
+        bzero(buffer,1024);
+        strcpy(buffer, "Hello.This is server, How you doing?");
+        printf("Server: %s\n", buffer);
+        send(client_sock, buffer, strlen(buffer), 0);
+
+        close(client_sock);
+        printf("Client disconnected\n");
+    }
+
+    return 0;
+}
